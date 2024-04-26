54d4a96433818c20eaab2d4076fcaedcece7c989
Jason5564 <96456170+Jason5564@users.noreply.github.com>
2024-04-26 21:25:01 +0800

    先複製gpt的code

--- predict_app/tests.py
+++ predict_app/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Post
 
-# Create your tests here.
+def post_list(request):
+    posts = Post.objects.all()
+    data = {"posts": list(posts.values())}
+    return JsonResponse(data)
+
+def post_detail(request, pk):
+    post = Post.objects.get(pk=pk)
+    data = {"post": {
+        "title": post.title,
+        "content": post.content,
+        "created_at": post.created_at
+    }}
+    return JsonResponse(data)
