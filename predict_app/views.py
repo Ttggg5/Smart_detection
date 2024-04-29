4f12cb4dc2b4e055784c023ced913ce7725a29df
Cheng Chain Lin <D1012337@o365.fcu.edu.tw>
2024-04-29 16:37:44 +0800

    add register.html

--- predict_app/views.py
+++ predict_app/views.py
@@ -4,3 +4,14 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'index.html')
+
+
+def login(request):
+    return render(request, 'login.html')
+
+
+def register(request):
+    if request.method == 'POST':
+        return render(request, 'register.html', {'state': '註冊成功', 'state_color': 'seagreen'})
+
+    return render(request, 'register.html')
--- smart_detection/urls.py
+++ smart_detection/urls.py
@@ -21,4 +21,6 @@ from predict_app import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('login/', views.login, name='login'),
+    path('register/', views.register, name='register'),
 ]
